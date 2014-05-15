527da4944f1b773e2d14e0851c9b1e0c6ffd109e
javathought <pascal.abaziou@gmail.com>
2014-05-16 01:37:30 +0200

    Pure blog initial version

---
+++ app/models/Post.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pascal
+ * Date: 16/05/14
+ * Time: 01:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Post {
+}
