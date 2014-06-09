ddda15ae68f650f71cfe9661125c53d9dc3a0dbe
javathought <pascal.abaziou@gmail.com>
2014-06-09 18:38:23 +0200

    Building the home screen

---
+++ app/Bootstrap.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pascal
+ * Date: 06/06/14
+ * Time: 00:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bootstrap {
+}
---
+++ app/models/Setting.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pascalabaziou
+ * Date: 07/06/2014
+ * Time: 10:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Setting {
+}
